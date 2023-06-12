b0928b7d9a831671a855831ea9c34a96367ac5b9
Moliki-Salman <salmanameenat@gmail.com>
2023-06-12 10:57:04 +0200

    some adjustment on the model

--- models/Blog.js
+++ models/Blog.js
@@ -2,24 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    username: {
-        type: String,
-    },
-    firstname: {
-        type: String, 
-    },
-    lastname: {
-        type: String, 
-    },
-    email: {
-        type: String, 
-    },
-    password: {
-        type: Number,
-    },
-    phone_number: {
-        type: Number,
-    },
+    title: String,
+    body: String,
+    image: String,
     createdAt: {
         type: Date,
         default: Date.now,
