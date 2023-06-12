90b981b10b0765a6888702f054e1b03d59b71b2b
Moliki-Salman <salmanameenat@gmail.com>
2023-06-12 10:50:21 +0200

    better arranagement of app.js

--- models/Blog.js
+++ models/Blog.js
@@ -2,13 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    title: String,
-    body: String,
-    image: String,
+    username: {
+        type: String,
+    },
+    firstname: {
+        type: String, 
+    },
+    lastname: {
+        type: String, 
+    },
+    email: {
+        type: String, 
+    },
+    password: {
+        type: Number,
+    },
+    phone_number: {
+        type: Number,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
     },
 });
 
-module.exports = mongoose.model("Blog", blogSchema);
+ module.exports = mongoose.model("Blog", blogSchema);
+
+
